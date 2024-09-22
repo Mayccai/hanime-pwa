c20cc2b8dfdc6bc24b7455fbcbc0be579eb78a68
Mayccai <108133179+Mayccai@users.noreply.github.com>
2024-09-22 15:37:46 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(event) {
+  console.log('Service worker installing.');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service worker activating.');
+});
